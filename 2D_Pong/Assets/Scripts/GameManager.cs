f9c255980abb86e5fa2a87690293acaefc361c88
marctex <marcelo.t.martins@hotmail.com>
2022-07-12 16:20:34 -0300

    adicionado função para reiniciar partida em GameManager e biblioteca UnityEngine.SceneManagement

--- 2D_Pong/Assets/Scripts/GameManager.cs
+++ 2D_Pong/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -20,7 +21,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.R))
+        {
+            GameStart();
+        }
     }
 
     public void IncreaseScorePlayerLeft()
@@ -39,5 +43,10 @@ public class GameManager : MonoBehaviour
     {
         score.text = pointsPlayerLeft + " X " + pointsPlayerRight;
         goal.Play();
+    }
+
+    private void GameStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
